49d33d3fcd604efa39ac995f60fb51b9b1fdf851
Mike <63655075+MixelTe@users.noreply.github.com>
2023-04-05 00:41:38 +0300

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager _inst;
+	[SerializeField] private GameField _gameField;
+
+	public static GameField GameField { get => _inst._gameField; }
+	[HideInInspector] public bool GameIsRunning = true;
+
+	private void Awake()
+	{
+		if (_inst != null) Destroy(gameObject);
+		else _inst = this;
+	}
+
+	public void OverGame()
+	{
+		GameIsRunning = false;
+		GameField.StopScrolling();
+	}
+}
