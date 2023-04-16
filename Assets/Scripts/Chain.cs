a37df19a2817c397b6b89122cf50de4f66b9b105
Mike <63655075+MixelTe@users.noreply.github.com>
2023-04-16 10:01:47 +0300

    Chain animation

--- Assets/Scripts/Chain.cs
+++ Assets/Scripts/Chain.cs
@@ -9,8 +9,9 @@ public class Chain : MonoBehaviour
 	[SerializeField] private float _strikeTime;
 	[SerializeField] private AnimationCurve _strikeScale;
 	[SerializeField] private AnimationCurve _strikeAlpha;
-	[SerializeField] private float _a;
-	[SerializeField] private float _b;
+	[SerializeField] private float _shakeSpeed;
+	[SerializeField] private float _shakeAmplitude;
+	[SerializeField] private Sprite[] _appearSprites;
 
 	public void Strike(float duration)
 	{
@@ -31,18 +32,29 @@ public class Chain : MonoBehaviour
 	{
 		var color = _renderer.color;
 		var pos = _chain.localPosition;
+		var rand = Random.value * Mathf.PI;
+		var shakeX = Random.value < 0.5f;
+		if (_appearSprites.Length > 0)
+			_renderer.sprite = _appearSprites[0];
 		for (float t = 0; t < 1; t += Time.fixedDeltaTime / duration)
 		{
 			color.a = Mathf.Lerp(0, 0.15f, t);
 			_renderer.color = color;
 
-			pos.x = Mathf.Sin(t * _a) * _b;
+			var shake = Mathf.Sin(t * _shakeSpeed + rand) * _shakeAmplitude;
+			if (shakeX) pos.x = shake;
+			else pos.y = shake;
 			_chain.localPosition = pos;
+
+			if (_appearSprites.Length > 0)
+				_renderer.sprite = _appearSprites[Mathf.FloorToInt(t * (_appearSprites.Length - 1))];
 
 			yield return new WaitForFixedUpdate();
 		}
 		color.a = 0;
 		_renderer.color = color;
+		if (_appearSprites.Length > 0)
+			_renderer.sprite = _appearSprites[^1];	
 	}
 
 	private IEnumerator StrikeAnim(float duration)
