8af61416392c4fc4e90e7fe8aa744d5d9efd73b1
Mike <63655075+MixelTe@users.noreply.github.com>
2023-05-20 12:27:23 +0300

    Improve sounds

--- Assets/Scripts/UI/SoundSetting.cs
+++ Assets/Scripts/UI/SoundSetting.cs
@@ -1,25 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SoundSetting : MonoBehaviour
 {
+    [SerializeField] private Slider _soundSlider;
     [SerializeField] private Slider _musicSlider;
-    [SerializeField] private Slider _allSounSlider;
 
     [SerializeField] private AudioMixer _audioMixer;
-	private Coroutine _sendMetrikaDelay;
-	private bool _sendMetrikaDisable = true;
+    [SerializeField] private AudioSource _soundChange;
+	private float _sendMetrikaDelay = -1;
+    private bool _disableOnChange = true;
+	private Coroutine _soundDelay;
 
     private void Start()
     {
         if (PlayerPrefs.HasKey(Settings.PlayerPrefs_SoundVolume))
         {
             var volume = PlayerPrefs.GetFloat(Settings.PlayerPrefs_SoundVolume);
-            _allSounSlider.value = volume;
+            _soundSlider.value = volume;
             SetSoundVolume(volume);
         }
 
@@ -29,15 +32,23 @@ public class SoundSetting : MonoBehaviour
             _musicSlider.value = volume;
             SetMusicVolume(volume);
         }
-        _sendMetrikaDisable = false;
+        _soundSlider.onValueChanged.AddListener(SetSoundVolume);
+        _musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        _disableOnChange = false;
     }
 
-    public void SetSoundVolume(float volume)
+    private void SetSoundVolume(float volume)
     {
         SetVolume("sounds", Settings.PlayerPrefs_SoundVolume, volume);
+        
+        if (_disableOnChange)
+            return;
+        if (_soundDelay != null)
+            StopCoroutine(_soundDelay);
+        _soundDelay = StartCoroutine(PlaySound());
     }
 
-    public void SetMusicVolume(float volume)
+    private void SetMusicVolume(float volume)
     {
         SetVolume("music", Settings.PlayerPrefs_MusicVolume, volume);
     }
@@ -61,17 +72,30 @@ public class SoundSetting : MonoBehaviour
         _audioMixer.SetFloat("master", 0);
     }
 
-    private void SendMetrika()
+    private async void SendMetrika()
 	{
-        if (_sendMetrikaDisable) return;
+        if (_disableOnChange)
+            return;
 
-        if (_sendMetrikaDelay != null)
-            StopCoroutine(_sendMetrikaDelay);
-        _sendMetrikaDelay = StartCoroutine(SendMetrikaDelay());
-	}
-    private IEnumerator SendMetrikaDelay()
-	{
-        yield return new WaitForSeconds(2);
+        var alreadyRan = _sendMetrikaDelay >= 0;
+        _sendMetrikaDelay = 2;
+        if (alreadyRan)
+            return;
+
+		while (_sendMetrikaDelay > 0)
+		{
+            _sendMetrikaDelay -= Time.deltaTime;
+            await Task.Yield();
+        }
+
         YaApi.MetrikaGoal(YaApi.MetrikaGoals.VolumeChanged);
+        _sendMetrikaDelay = -1;
+    }
+
+    private IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(0.15f);
+        _soundChange.Play();
+        _soundDelay = null;
     }
 }
